1fa59cd8c55a220812e194e6446089a23a465241
aname <47141166+as06zx@users.noreply.github.com>
2022-07-16 04:32:48 +0900

    Added missing string

--- Obfuscation.lua
+++ Obfuscation.lua
@@ -1,4 +1,5 @@
 --add your code between [[ ]] to encode it
+
 -- byte obfuscation
 local byteob = [[
 print("Hello World!")
@@ -10,13 +11,8 @@ print("Hello World!")
 
 local encoded = byteob:gsub(".", function(bb) return "\\" .. bb:byte() end) or byteob .. "\""
 
-local yourobfuscatedbytecode = [[
-"\112\114\105\110\116\40\34\72\101\108\108\111\32\87\111\114\108\100\33\34\41\10"
-]]
-
-
 print("Byte Obfuscation output:")
 print("loadstring(\""..encoded.."\")()")
 
 print("Encode Function output:")
-print(encodeFunction(load(encodefunc)))
+print("decodeFunction(\""..encodeFunction(load(encodefunc)).."\")()")
